3f7c49f87dd58789b422003037e021c161013809
Robinson <nosnibor1989@gmail.com>
2017-05-11 16:59:57 -0300

    First route

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,12 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+// App Home
+$app->get('/', function ($request, $response, $args) {
+
+    $data = array('API' => 'Payment-Gateway-Flow');
+
+    return $response->withJson($data);
+
 });
