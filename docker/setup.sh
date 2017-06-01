7cd8928c6ceda17cbc8dee11a326ed940d16a189
Marcos Duarte <maarcos.d@gmail.com>
2017-06-01 23:09:10 +1000

    Update scripts to run composer install

--- docker/setup.sh
+++ docker/setup.sh
@@ -13,7 +13,18 @@ sleep 2
 docker build -t shareableinnovations/php7 image > /dev/null 2>&1
 stop_spinner $?
 
-start_spinner 'Building docker image...'
+start_spinner 'Spinning up containers...'
 sleep 2
+docker-compose down > /dev/null 2>&1
 docker-compose up -d > /dev/null 2>&1
 stop_spinner $?
+
+start_spinner 'Running `composer install`...'
+sleep 2
+docker exec -i php /bin/bash -c 'composer install' > /dev/null 2>&1
+stop_spinner $?
+
+echo '
+'
+echo 'Make sure you set the virtual host by running the following command:'
+echo 'echo "$(docker-machine ip default) payment-flow.dev" | sudo tee -a /etc/hosts'
